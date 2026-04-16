417bc484b13e644e398a100e51d3fa229abec9d8
Ahmed-Erjaila <220202901@ostimteknik.edu.tr>
2026-04-16 23:58:07 +0300

    Update Smart_EMF_Detector.ino

--- Smart_EMF_Detector.ino
+++ Smart_EMF_Detector.ino
@@ -7,7 +7,7 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 // Hardware mapping
 const int coilPin = A0;
 const int ledPin = 13;
-const int buzzerPin = 8;
+const int speakerPin = 8;
 const int buttonPin = 7;
 const int irPin = 10;
 
@@ -16,15 +16,14 @@ bool systemActive = false;
 bool calibrated = false;
 bool lastBtnState = HIGH;
 
-void setup() {
-  Serial.begin(9600); // Added for debugging like a real dev
-  lcd.init();
-  lcd.noBacklight();
-  
-  pinMode(ledPin, OUTPUT);
-  pinMode(buzzerPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
-  IrReceiver.begin(irPin);
-  
-  Serial.println("System initiated..."); 
+// Signal processing for speaker output
+void playSpeaker(int frequency, int duration) {
+  long period = 1000000L / frequency;
+  long startTime = millis();
+  while (millis() - startTime < duration) {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(period / 2);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(period / 2);
+  }
 }
